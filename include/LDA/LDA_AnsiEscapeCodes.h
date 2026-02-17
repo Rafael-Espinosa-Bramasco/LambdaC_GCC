c459f86922494437176da1639749e81a02d2b64b
Rafael-Espinosa-Bramasco <rafaskrullantonio@gmail.com>
2026-02-17 10:53:30 -0600

    [MODIFIED] ANSI escape codes finished (few codes, not all the codes in ANSI documentation implemented)

--- include/LDA/LDA_AnsiEscapeCodes.h
+++ include/LDA/LDA_AnsiEscapeCodes.h
@@ -8,6 +8,17 @@ typedef enum
     LDA_AnsiClearEntire,
     LDA_AnsiClearEntireAndRestart,
 }LDA_AnsiClearOptions;
+typedef enum
+{
+    LDA_AnsiColorBlack = 30,
+    LDA_AnsiColorRed,
+    LDA_AnsiColorGreen,
+    LDA_AnsiColorYellow,
+    LDA_AnsiColorBlue,
+    LDA_AnsiColorMagenta,
+    LDA_AnsiColorCyan,
+    LDA_AnsiColorWhite,
+}LDA_AnsiColorOptions;
 
 void LDA_AnsiCursorUp(unsigned int times);
 void LDA_AnsiCursorDown(unsigned int times);
@@ -21,8 +32,22 @@ void LDA_AnsiClearScreen(LDA_AnsiClearOptions option);
 void LDA_AnsiClearLine(LDA_AnsiClearOptions option);
 void LDA_AnsiScrollUp(unsigned int lines);
 void LDA_AnsiScrollDown(unsigned int lines);
-void LDA_AnsiHideCursor();
-void LDA_AnsiShowCursor();
-void LDA_AnsiResetVisualStyle();
+void LDA_AnsiHideCursor(void);
+void LDA_AnsiShowCursor(void);
+void LDA_AnsiResetVisualStyle(void);
+void LDA_AnsiSetFontBolt(void);
+void LDA_AnsiSetFontFaint(void);
+void LDA_AnsiSetFontItalic(void);
+void LDA_AnsiSetFontUnderline(void);
+void LDA_AnsiSetFontNormalIntensity(void);
+void LDA_AnsiSetFontNotItalic(void);
+void LDA_AnsiSetFontNotUnderline(void);
+void LDA_AnsiSetFontColor(LDA_AnsiColorOptions colorOption);
+void LDA_AnsiSetFontColorRGB(unsigned char r, unsigned char g, unsigned char b);
+void LDA_AnsiResetFontColor(void);
+void LDA_AnsiSetBackgroundColor(LDA_AnsiColorOptions colorOption);
+void LDA_AnsiSetBackgroundColorRGB(unsigned char r, unsigned char g, unsigned char b);
+void LDA_AnsiResetBackgroundColor(void);
+void LDA_AnsiInvertVideoStyle(void);
 
 #endif /* LDA_C_ANSIESCAPECODES_INCLUDED */
--- src/LDA_AnsiEscapeCodes.c
+++ src/LDA_AnsiEscapeCodes.c
@@ -54,15 +54,71 @@ void LDA_AnsiScrollDown(unsigned int lines)
 {
     printf("\033[%iT",lines);
 }
-void LDA_AnsiHideCursor()
+void LDA_AnsiHideCursor(void)
 {
     printf("\033[?25l");
 }
-void LDA_AnsiShowCursor()
+void LDA_AnsiShowCursor(void)
 {
     printf("\033[?25h");
 }
-void LDA_AnsiResetVisualStyle()
+void LDA_AnsiResetVisualStyle(void)
 {
     printf("\033[0m");
+}
+void LDA_AnsiSetFontBolt(void)
+{
+    printf("\033[1m");
+}
+void LDA_AnsiSetFontFaint(void)
+{
+    printf("\033[2m");
+}
+void LDA_AnsiSetFontItalic(void)
+{
+    printf("\033[3m");
+}
+void LDA_AnsiSetFontUnderline(void)
+{
+    printf("\033[4m");
+}
+void LDA_AnsiSetFontNormalIntensity(void)
+{
+    printf("\033[22m");
+}
+void LDA_AnsiSetFontNotItalic(void)
+{
+    printf("\033[23m");
+}
+void LDA_AnsiSetFontNotUnderline(void)
+{
+    printf("\033[24m");
+}
+void LDA_AnsiSetFontColor(LDA_AnsiColorOptions colorOption)
+{
+    printf("\033[%im",colorOption);
+}
+void LDA_AnsiSetFontColorRGB(unsigned char r, unsigned char g, unsigned char b)
+{
+    printf("\033[38;2;%i;%i;%im",r,g,b);
+}
+void LDA_AnsiResetFontColor(void)
+{
+    printf("\033[39m");
+}
+void LDA_AnsiSetBackgroundColor(LDA_AnsiColorOptions colorOption)
+{
+    printf("\033[%im",colorOption+10);
+}
+void LDA_AnsiSetBackgroundColorRGB(unsigned char r, unsigned char g, unsigned char b)
+{
+    printf("\033[48;2;%i;%i;%im",r,g,b);
+}
+void LDA_AnsiResetBackgroundColor(void)
+{
+    printf("\033[49m");
+}
+void LDA_AnsiInvertVideoStyle(void)
+{
+    printf("\033[7m");
 }
